f011230aca70c6dd4769bad1862c0da725630423
esfox <osairis100@gmail.com>
2022-04-03 07:12:44 +0800

    Update transaction list amount colors

--- src/components/Index/TransactionListItem.svelte
+++ src/components/Index/TransactionListItem.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import classNames from 'classnames';
+
   import { formatCurrency, formatDate } from 'src/helpers/common';
 
   export let amount = 0;
@@ -10,7 +12,9 @@
 </script>
 
 <tr class="w-full items-center not-last:(border-b border-b-light)-900">
-  <td class="w-[10rem] p-4">{amountString}</td>
+  <td class={classNames('w-[10rem] p-4', amount > 0 ? 'text-green-600' : 'text-red-600')}
+    >{amountString}</td
+  >
   <td>{title}</td>
   <td class="w-[10rem]">
     <small class="text-dark-50">{dateString}</small>
