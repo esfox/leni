fef0a5cc2e57a9b827e29ccefa55d4681b12160d
esfox <osairis100@gmail.com>
2022-04-04 07:58:34 +0800

    Fix logo typo

--- src/components/Logo.svelte
+++ src/components/Logo.svelte
@@ -1,5 +1,5 @@
 <h1 class="text-4xl font-bold">Leni</h1>
 <small class="mt-1">
   <strong>L</strong>ist of <strong>E</strong>xpenses '<strong>n</strong>
-  <strong>I</strong>ncomes
+  <strong>I</strong>ncome
 </small>
