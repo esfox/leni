be3b19bf9bdfa0ea5d2d3982ae06047d9a6d7601
esfox <osairis100@gmail.com>
2022-04-04 10:47:36 +0800

    Use Node adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
 import WindiCSS from 'vite-plugin-windicss';
 import path from 'path';
@@ -10,7 +10,7 @@ const config = {
   preprocess: preprocess(),
 
   kit: {
-    adapter: adapter(),
+    adapter: adapter({ out: 'build' }),
     vite: {
       plugins: [
         WindiCSS(),
