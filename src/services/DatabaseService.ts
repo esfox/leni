fe14793eb54be1dfe71ac37495f85dc988061ed8
esfox <osairis100@gmail.com>
2022-04-04 10:54:15 +0800

    Fix knex issue

--- src/services/DatabaseService.ts
+++ src/services/DatabaseService.ts
@@ -1,11 +1,14 @@
 import { env } from '$lib/env';
-import { knex } from 'knex';
+import pkg from 'knex';
 import pg from 'pg';
 
 pg.types.setTypeParser(pg.types.builtins.INT8, (value: string) => Number(value));
 pg.types.setTypeParser(pg.types.builtins.FLOAT8, (value: string) => Number(value));
 pg.types.setTypeParser(pg.types.builtins.NUMERIC, (value: string) => Number(value));
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-ignore
+const { knex } = pkg;
 export const database = knex({
   client: 'pg',
   connection: env.DATABASE_CONNECTION_STRING,
